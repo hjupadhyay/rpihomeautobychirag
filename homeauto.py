91d7867de67fa36f8ca017be6d59ac1347b086ee
Harshit Upadhyay <hjupadhyay@gmail.com>
2013-11-18 18:03:52 +0530

    intial commit

---
+++ homeauto.py
@@ -0,0 +1,4 @@
+# homeauto.py the main file to run the raspberry pi automation system
+# current dummy program only outputs text for the action taken
+# displays device state as text only
+print "Hello this is the RPi Home Automation program";
